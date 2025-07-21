74adda3f0406746a94d60529dd0b3969a61c1fab
Sulafa <sulafa.ssm@gmail.com>
2025-07-21 21:15:35 +0300

    Initial commit

---
+++ Skinet.Infrastructure/Persistence/Configuration/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skinet.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skinet.Infrastructure.Persistence.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+  public  void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+           
+
+        }
+    }
+}
---
+++ Skinet.Infrastructure/Persistence/SkinetDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Skinet.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skinet.Infrastructure.Persistence
+{
+    public  class SkinetDbContext : DbContext
+    {
+        public SkinetDbContext(DbContextOptions<SkinetDbContext> options) : base(options)
+        {      
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+           
+            base.OnModelCreating(modelBuilder);
+          
+        }
+    }
+   
+}
