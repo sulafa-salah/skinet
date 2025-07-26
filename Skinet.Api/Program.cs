840c51dbe5fefc094158629d67774273a3106b9f
Sulafa <sulafa.ssm@gmail.com>
2025-07-26 16:27:48 +0300

    chore(cors): added CORS policy for localhost development

--- Skinet.Api/Program.cs
+++ Skinet.Api/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +31,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthorization();
 
